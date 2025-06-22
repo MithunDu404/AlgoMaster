81bc9f385899e3f1d1c0e03ff1c7970a97d61e15
Mithun Dutta <mithundu632@gmail.com>
2025-06-22 21:46:59 +0530

    added hover animation to nav-links

---
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import './Dashboard.css';
+
+function Dashboard(){
+    return(
+        <div className="moto">
+            <h1>AlgoMaster</h1>
+            <h1>AlgoMaster</h1>
+            <h1>AlgoMaster</h1>
+            <h1>AlgoMaster</h1>
+            <p></p>
+        </div>
+    )
+}
+
+export default Dashboard;
